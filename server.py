4261d7b8c6323db917ee395b79a07c64aa6eefe3
crownedsilverfox <byskillcfpro@gmail.com>
2017-05-20 20:43:54 +0700

    Crypto errors improved

--- server.py
+++ server.py
@@ -181,7 +181,7 @@ class Game:
     def decrypt(self, message):
         message = b64decode(message)
         err = None
-        dec_message = json.loads(self.cipher.decrypt(message, err).decode(), encoding='utf-8')
+        dec_message = json.loads(self.cipher.decrypt(message, err).decode(encoding='utf-8'))
         return dec_message
 
 
